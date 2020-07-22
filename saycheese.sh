df00e8d2962e31de424c784dfd724def973e8678
MRATIQ420 <67003289+MRATIQ420@users.noreply.github.com>
2020-07-23 00:39:04 +0500

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -17,8 +17,8 @@ printf "      \e[1;92m              |___/ \e[0m                                 
 
 printf "\n"                                                              
 printf "\e[1;77m               .:.:\e[0m\e[1;93m Grab webcam shots by link \e[0m\e[1;77m:.:.\e[0m\n"                              
-printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]              v1.2 coded by @linux_choice              \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
-printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]          github.com/thelinuxchoice/saycheese          \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]              v1.2 coded by @Black_Ranjha             \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]   youtube :   https://www.youtube.com/channel/UCPjsRDBYr8KIR-3iMs00zmg       \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
 printf "\n"
 printf "        \e[1;91m Disclaimer: this tool is designed for security\n"
 printf "         testing in an authorized simulated cyberattack\n"
